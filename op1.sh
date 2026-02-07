c3cac0be4fec967967bbd3a1bfba19863d3bc4a8
haoran <lxhao61@gmail.com>
2026-02-08 06:25:52 +0800

    Add files via upload

--- op1.sh
+++ op1.sh
@@ -10,17 +10,26 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 查看所有标签
+#git tag
+# 切换到标签 v23.05.6
+git checkout v23.05.6
+
+# 回退源码
+#git reset --hard 1c26bcb #等同于切换到标签 v23.05.2
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages.git;main' >>feeds.conf.default
-#echo 'src-git homeproxy https://github.com/lxiaya/openwrt-homeproxy.git' >>feeds.conf.default
-#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-#echo 'src-git clash https://github.com/lxiaya/onecloud-package' >>feeds.conf.default
-#echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/lxiaya/helloworld' >>feeds.conf.default
-echo 'net.netfilter.nf_conntrack_max=65536' >>package/kernel/linux/files/sysctl-nf-conntrack.conf
-echo 'net.core.default_qdisc=fq' >>package/kernel/linux/files/sysctl-tcp-bbr.conf
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# 注释默认 packages
+sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
+# 添加 Immortal 大 packages
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git;openwrt-23.05' feeds.conf.default
+#sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^1820d9b' feeds.conf.default
+
+# 注释默认 luci
+sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
+# 添加 Immortal 大 luci
+sed -i '$a src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05' feeds.conf.default
