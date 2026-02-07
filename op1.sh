9fffd7668471d28a303d0c6a0e1c00c43abf911e
haoran <lxhao61@gmail.com>
2026-02-08 06:29:26 +0800

    Add files via upload

--- op1.sh
+++ op1.sh
@@ -10,17 +10,46 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 查看所有标签
+#git tag
+# 切换到标签 v23.05.6
+git checkout v23.05.6
+
+# 回退源码
+#git reset --hard f372b71 #等同于切换到标签 v22.03.6
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages.git;main' >>feeds.conf.default
-#echo 'src-git homeproxy https://github.com/lxiaya/openwrt-homeproxy.git' >>feeds.conf.default
-#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-#echo 'src-git clash https://github.com/lxiaya/onecloud-package' >>feeds.conf.default
-#echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/lxiaya/helloworld' >>feeds.conf.default
-echo 'net.netfilter.nf_conntrack_max=65536' >>package/kernel/linux/files/sysctl-nf-conntrack.conf
-echo 'net.core.default_qdisc=fq' >>package/kernel/linux/files/sysctl-tcp-bbr.conf
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# 注释默认 packages
+#sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
+# 添加 packages
+#sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;23.05' feeds.conf.default
+#sed -i '$a src-git packages https://github.com/openwrt/packages.git^b5ed85f6e94aa08de1433272dc007550f4a28201' feeds.conf.default
+
+# 注释默认 luci
+#sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
+# 添加 luci
+#sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;23.05' feeds.conf.default
+#sed -i '$a src-git luci https://github.com/openwrt/luci.git^63ba3cba5b7bfb803a875d4d8f01248634687fd5' feeds.conf.default
+
+# 注释默认 routing
+#sed -i 's/^\(.*routing\)/#&/' feeds.conf.default
+# 添加 routing
+#sed -i '$a src-git routing https://github.com/Lienol/openwrt-routing.git;23.05' feeds.conf.default
+#sed -i '$a src-git routing https://github.com/openwrt/routing.git^e351d1e623e9ef2ab78f28cb1ce8d271d28c902d' feeds.conf.default
+
+# 注释默认 telephony
+#sed -i 's/^\(.*telephony\)/#&/' feeds.conf.default
+# 添加 telephony
+#sed -i '$a src-git telephony https://github.com/Lienol/openwrt-telephony.git;23.05' feeds.conf.default
+#sed -i '$a src-git telephony https://github.com/openwrt/telephony.git^98c8a5aa4624312ed758e2e2b6d4039050a1649d' feeds.conf.default
+
+# 添加 lienol 大的 package
+echo 'src-git lienol1 https://github.com/Lienol/openwrt-package.git;main' >>feeds.conf.default
+echo 'src-git lienol2 https://github.com/Lienol/openwrt-package.git;other' >>feeds.conf.default
+
+# 添加 luci-app-msd_lite
+#echo 'src-git msd_lite https://github.com/iii80/luci-app-msd.git' >>feeds.conf.default
--- op3.sh
+++ op3.sh
@@ -10,18 +10,84 @@
 # Description: OpenWrt DIY script part 2 (rm Update feeds)
 #
 
-#=================================================
-#sudo rm -rf feeds/packages/net/sing-box
-#sudo rm -rf feeds/packages/net/microsocks
-#sudo rm -rf feeds/packages/net/trojan-go
-sudo rm -rf feeds/packages/net/v2raya
-git clone https://github.com/orgx2812/v2raya feeds/packages/net/v2raya
-#sudo rm -rf feeds/packages/net/v2ray-core
-sudo rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/orgx2812/v2ray-geodata feeds/packages/net/v2ray-geodata
-sudo rm -rf feeds/packages/net/xray-core
-git clone https://github.com/orgx2812/xray-core feeds/packages/net/xray-core
-#sudo rm -rf feeds/packages/net/chinadns-ng
-#sudo rm -rf feeds/luci/applications/luci-app-homeproxy
-sudo rm -rf feeds/packages/lang/golang
-git clone https://github.com/orgx2812/golang feeds/packages/lang/golang
+# 删除自带的 golang
+rm -rf feeds/packages/lang/golang
+# 拉取新的 golang
+git clone https://github.com/sbwml/packages_lang_golang.git -b 25.x feeds/packages/lang/golang
+
+# 删除 passwall 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf package/feeds/packages/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+# 拉取新的 passwall-packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
+#cd package/passwall-packages
+#git checkout bc40fceb0488dfb5a4adb711cc1830a8021ee555
+#cd -
+
+# 删除 passwall 过时的 luci
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/luci/luci-app-passwall
+# 拉取新的 passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git package/passwall-luci
+#cd package/passwall-luci
+#git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
+#cd -
+
+# 拉取 ShadowSocksR Plus+
+#git clone https://github.com/fw876/helloworld.git -b master package/helloworld
+
+# 拉取锐捷认证
+git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
+
+# 拉取 easytier、luci-app-easytier
+git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
+
+# 拉取 msd_lite、luci-app-msd_lite
+git clone https://github.com/gtolog/openwrt-msd_lite.git package/msd_lite
+#git clone https://github.com/gw826943555/openwrt_msd_lite.git package/msd_lite
+
+# 拉取 OpenAppFilter、luci-app-oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# 拉取 luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci/luci-theme-argon
+
+# 删除自带的 ddns-scripts
+rm -rf feeds/packages/net/ddns-scripts
+# 删除自带的 rust
+#rm -rf feeds/packages/lang/rust
+# 删除自带的 luci-app-socat
+#rm -rf feeds/lienol1/luci-app-socat
+# 删除自带的 tailscale
+rm -rf feeds/packages/net/tailscale
+rm -rf package/feeds/packages/tailscale
+
+# 筛选程序
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+# 提取 ddns-scripts
+merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git feeds/packages/net net/ddns-scripts
+# 提取 rust
+#merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git feeds/packages/lang lang/rust
+# 提取 luci-app-socat
+#merge_package main https://github.com/chenmozhijin/luci-app-socat.git feeds/lienol1 luci-app-socat
+# 提取 pdnsd-alt、upx、tailscale、luci-app-tailscale
+merge_package main https://github.com/kenzok8/small-package.git package/small-package pdnsd-alt upx tailscale luci-app-tailscale
+# 提取 luci-theme-argon
+merge_package openwrt-23.05 https://github.com/sbwml/luci-theme-argon.git package/luci luci-theme-argon
