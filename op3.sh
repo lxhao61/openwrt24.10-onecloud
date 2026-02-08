0561c5a8a0391b554ef38226f44fe96344355f98
haoran <lxhao61@gmail.com>
2026-02-08 14:50:55 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -14,8 +14,8 @@
 cp package/kernel/linux/modules/crypto.mk package/kernel/linux/modules/crypto.mk.bak
 wget https://raw.githubusercontent.com/openwrt/openwrt/openwrt-23.05/package/kernel/linux/modules/crypto.mk \
   -O package/kernel/linux/modules/crypto.mk
-echo '# CONFIG_CRYPTO_SHA1_ARM is not set' >> target/linux/amlogic/meson8b/config-5.15
-echo '# CONFIG_CRYPTO_SHA1_ARM_NEON is not set' >> target/linux/amlogic/meson8b/config-5.15
+#echo '# CONFIG_CRYPTO_SHA1_ARM is not set' >> target/linux/amlogic/meson8b/config-5.15
+#echo '# CONFIG_CRYPTO_SHA1_ARM_NEON is not set' >> target/linux/amlogic/meson8b/config-5.15
 
 # 删除自带的 golang
 rm -rf feeds/packages/lang/golang
