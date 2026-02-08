f29c431dd119643de44150e713616665350a5bcd
haoran <lxhao61@gmail.com>
2026-02-08 08:04:30 +0800

    Update op1.sh

--- op1.sh
+++ op1.sh
@@ -22,14 +22,3 @@ git checkout v23.05.6
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# 注释默认 packages
-sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
-# 添加 Immortal 大 packages
-sed -i '$a src-git packages https://github.com/immortalwrt/packages.git;openwrt-23.05' feeds.conf.default
-#sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^1820d9b' feeds.conf.default
-
-# 注释默认 luci
-sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
-# 添加 Immortal 大 luci
-sed -i '$a src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05' feeds.conf.default
